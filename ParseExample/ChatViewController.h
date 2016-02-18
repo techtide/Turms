0c9bb0beb59a75a5ddd7cb1129f708ec67692b05
Arman <armanb21@users.noreply.github.com>
2016-02-18 22:07:57 +0000

    Revert "Revert "Backup before El Capitane'""
    This reverts commit fe70644db3ea2a7f3b1a50bafa1d26d40b2f79de.

--- ParseExample/ChatViewController.h
+++
@@ -1,9 +0,0 @@
-//fork availible
-#import <UIKit/UIKit.h>
-#import "ParseExampleAppDelegate.h"
-#import <Parse/Parse.h>
-@interface ChatViewController : UIViewController
-@property PFObject *parseMessage;
-
-
-@end
